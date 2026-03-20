f0eab7d6e0ec2aa784822e6e9a98d9a0b4f168d1
liu7d7 <bobg2488@gmail.com>
2026-03-20 12:25:45 -0700

    bugfix: css padding on sides, width bug in sa

--- conv_utf8.py
+++ conv_utf8.py
@@ -1,17 +1,16 @@
 import os
 import re
 
-for root, dirs, files in os.walk('.'):
-  for directory in dirs + ['.']:
-    for fi in os.scandir(directory):
-      if fi.is_file() and fi.name.endswith(".html"):
-        # print(directory + '\\' + fi.name)
-        fn = directory + '/' + fi.name
-        with open(fn, "r") as f:
-          try:
-            file_string = f.read()
-            file_string = re.sub("\\\\U([0-9a-fA-F]+)", "&#x\\1;", file_string).replace("<br/> ", "<br/>").replace("\\t", "  ").replace("\\T", "\t").replace("\\/", "/")
-          except:
-            print(f"error file {fn}")
-        with open(fn, "w") as f:
-          f.write(file_string)
+for directory in ['.', 'songs', 'pages']:
+  for fi in os.scandir(directory):
+    if fi.is_file() and fi.name.endswith(".html"):
+      # print(directory + '\\' + fi.name)
+      fn = directory + '/' + fi.name
+      with open(fn, "r") as f:
+        try:
+          file_string = f.read()
+          file_string = re.sub("\\\\U([0-9a-fA-F]+)", "&#x\\1;", file_string).replace("<br/> ", "<br/>").replace("\\t", "  ").replace("\\T", "\t").replace("\\/", "/")
+        except:
+          print(f"error file {fn}")
+      with open(fn, "w") as f:
+        f.write(file_string)
