e888bbef2d6e9aec1645b5ed9a020fc38b0cd0e7
liu7d7 <bobg2488@gmail.com>
2026-03-20 11:27:58 -0700

    feat: migrate from old react blog

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+xcopy .\assets .\final\assets\ /s /e /y
+copy .\CNAME .\final\CNAME /y
+
+echo preprocessor
+
+for %%I in (*.html) do if not "%%I" == "basic.html" cpp %%I -I. -P  -finput-charset=UTF-8 -fexec-charset=UTF-8 -o .\final\%%I --no-warnings
+
+pushd songs
+for %%I in (*.html) do if not "%%I" == "basic.html" cpp %%I -I.. -P  -finput-charset=UTF-8 -fexec-charset=UTF-8 -o ..\final\songs\%%I --no-warnings
+popd
+
+pushd pages
+for %%I in (*.html) do if not "%%I" == "basic.html" cpp %%I -I.. -P  -finput-charset=UTF-8 -fexec-charset=UTF-8 -o ..\final\pages\%%I --no-warnings
+popd
+
+echo convert utf-8
+
+pushd final
+py ..\conv_utf8.py
+popd 
+
+echo done!
